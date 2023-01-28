99389b2e8e162423875f1be0ef47fb252fad92ea
andru666 <92786858+andru666@users.noreply.github.com>
2023-01-28 10:20:25 +0300

    open csv

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ import traceback
 import os, sys, glob
 
 __all__ = 'install_android'
-__version__ = '0.10.31'
+__version__ = '0.10.30'
 
 if mod_globals.os == 'android':
     fs = fs*2
@@ -66,7 +66,7 @@ if mod_globals.os == 'android':
         Uri = autoclass("android.net.Uri")
         
         from android.permissions import request_permissions, Permission
-        request_permissions([Permission.READ_EXTERNAL_STORAGE])
+        request_permissions([Permission.READ_EXTERNAL_STORAGE,Permission.BLUETOOTH_CONNECT,Permission.BLUETOOTH_SCAN])
 
         if api_version > 29:
             # If you have access to the external storage, do whatever you need
@@ -274,11 +274,11 @@ class PYDDT(App):
             popup_init = Popup(title=LANG.l_load, title_size=fs*1.5, title_align='center', content=lbltxt, size=(Window.size[0]*0.8, Window.size[1]*0.8), size_hint=(None, None))
             popup_init.open()
             base.EventLoop.idle()
+            sys.stdout.flush()
             popup_init.dismiss()
-            base.EventLoop.window.canvas.clear()
             self.stop()
+            base.EventLoop.window.canvas.clear()
             mod_ddt.DDT_START(mod_globals.opt_car, self.elm)
-            base.EventLoop.idle()
         else:
             popup.open()
             return
