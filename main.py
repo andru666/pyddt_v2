322cbdca38fdf82e9ded1e6b835ee1ba046d89a9
andru666 <92786858+andru666@users.noreply.github.com>
2023-03-03 20:10:17 +0300

    version

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ import traceback
 import os, sys, glob
 
 __all__ = 'install_android'
-__version__ = '0.12.15'
+__version__ = '0.12.16'
 
 if mod_globals.os == 'android':
     fs = fs*2
@@ -302,7 +302,7 @@ class PYDDT(App):
         root = ScrollView(size_hint=(1, 1))
         root.add_widget(layout)
         Fl.add_widget(root)
-        Fl.add_widget(MyLabel(text='Version : ' + __version__ , size_hint =(.3, None), pos=(0, Window.size[1]-title.height/1.5), font_size=(fs*0.8), height=fs*1.4, multiline=True))
+        Fl.add_widget(MyLabel(text='Version : ' + __version__ , size_hint =(.3, None), pos=(0, Window.size[1]-title.height/2), font_size=(fs*0.8), height=fs*1.4, multiline=True))
         return Fl
 
     def scanALLecus(self, instance):
